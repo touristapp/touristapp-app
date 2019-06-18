35ce975d29ecc5601764433ff4d2f42c433484bb
Pierre Herisse <pierre.herisse@gmail.com>
2019-06-18 10:12:05 +0200

    Add react-native-paper design componemts

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import AppNavigator from './src/navigation';
 export default class App extends React.Component {
   render() {
     return (
-      <AppNavigator ref={navigatorRef => this.navigatorRef = navigatorRef} />
+        <AppNavigator ref={navigatorRef => this.navigatorRef = navigatorRef} />
     );
   }
 }
