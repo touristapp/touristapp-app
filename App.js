420e90aed785b09278181482453f4cc968a7f65f
Seedockh <pierre.herisse@gmail.com>
2019-06-18 15:42:59 +0200

    Login almost done + Add RootContext

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Home from './src/screens/Home';
+import Login from './src/screens/Auth/Login';
 
 export default class App extends React.Component {
   render() {
     return (
-        <Home />
+        <Login />
     );
   }
 }
